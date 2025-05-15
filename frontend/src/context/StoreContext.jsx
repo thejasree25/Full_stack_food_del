9e681fedca0779b5866ba6f5f3a72cb225bd9506
thejasree25 <g.thejasree15@gmail.com>
2025-05-15 17:34:38 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ import axios from "axios"
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://full-stack-food-del.onrender.com"
+  const url = "https://full-stackfooddelbackend.onrender.com"
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([])
   useEffect(() => {
